68149a55e410446bb8b723a25f5a318adc71e639
hjyjava <287163157@qq.com>
2016-04-14 17:38:00 +0800

    company 16-4-14

--- app/src/main/java/com/huang/superbracelet/activity/home/MainActivity.java
+++ app/src/main/java/com/huang/superbracelet/activity/home/MainActivity.java
@@ -1,15 +1,20 @@
 package com.huang.superbracelet.activity.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.huang.superbracelet.R;
+import com.huang.superbracelet.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initWedgit() {
+        super.initWedgit();
     }
 }
--- app/src/main/java/com/huang/superbracelet/base/BaseActivity.java
+++ app/src/main/java/com/huang/superbracelet/base/BaseActivity.java
@@ -1,7 +1,59 @@
 package com.huang.superbracelet.base;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.huang.superbracelet.utils.MyToastUtils;
+import com.huang.superbracelet.utils.NetUtils;
+
 /**
  * Created by 黄家远 on 16/4/14.
  */
-public class BaseActivity {
+public class BaseActivity extends AppCompatActivity{
+    private int userId;
+    private String token;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initIntent();
+        initIntance();
+        initWedgit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void initIntent(){
+
+    }
+
+    protected void initIntance(){
+
+    }
+
+    protected void initWedgit(){
+
+    }
+
+    protected void initData(){
+
+    }
+
+    protected void initDataFromDB(){
+
+    }
+
+    protected void initDataFromHttp(){
+        if(NetUtils.isConnected(this))
+            MyToastUtils.showShort(this,"网络未连接");
+            return;
+    }
 }
